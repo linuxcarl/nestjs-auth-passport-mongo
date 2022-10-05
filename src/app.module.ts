643feff7d818144f706e30356402b1e41c593622
Carlos Ramirez <carlos@extrasistemas.com>
2022-10-04 23:21:43 -0500

    added test by global modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
 import { UsersModule } from './users/users.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [ProductsModule, UsersModule],
+  imports: [ProductsModule, UsersModule, DatabaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
