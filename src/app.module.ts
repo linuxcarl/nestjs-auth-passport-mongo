f28ef1bc1011df77deb535f41471a8ceae05299b
Carlos Ramirez <carlos@extrasistemas.com>
2022-10-06 09:36:43 -0500

    added validation with Joi by schemas .envs

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,7 @@ import { DatabaseModule } from './database/database.module';
 import { ConfigModule } from '@nestjs/config';
 import { environment } from '../envirnoments';
 import config_enviroments from './config_enviroments';
+import * as Joi from 'joi';
 
 @Module({
   imports: [
@@ -17,6 +18,14 @@ import config_enviroments from './config_enviroments';
       envFilePath: environment[process.env.NODE_ENV] || '.env',
       load: [config_enviroments],
       isGlobal: true,
+      validationSchema: Joi.object({
+        DB_NAME: Joi.string().required(),
+        DB_HOST: Joi.string().required(),
+        DB_PORT: Joi.number().required(),
+        DB_USER: Joi.string().required(),
+        DB_PASS: Joi.string().required(),
+        API_KEY: Joi.string().required(),
+      }),
     }),
   ],
   controllers: [AppController],
