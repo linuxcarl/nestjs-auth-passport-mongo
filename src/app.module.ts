a6e25c3cabf2990bc33b33e54fabcf10d99b76c2
Carlos Ramirez <carlos@extrasistemas.com>
2022-10-06 08:43:03 -0500

    added envirnoments by fiels .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,19 @@ import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
 import { UsersModule } from './users/users.module';
 import { DatabaseModule } from './database/database.module';
+import { ConfigModule } from '@nestjs/config';
+import { environment } from '../envirnoments';
 
 @Module({
-  imports: [ProductsModule, UsersModule, DatabaseModule],
+  imports: [
+    ProductsModule,
+    UsersModule,
+    DatabaseModule,
+    ConfigModule.forRoot({
+      envFilePath: environment[process.env.NODE_ENV] || '.env',
+      isGlobal: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,9 +1,17 @@
 import { Inject, Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AppService {
-  constructor(@Inject('API_KEY') private apiKey: string) {}
+  constructor(
+    @Inject('API_KEY') private apiKey: string,
+    private readonly configService: ConfigService,
+  ) {}
   getHello(): string {
-    return `api key=> ${this.apiKey}`;
+    return `api key=> ${
+      this.apiKey
+    }\n- DB_NAME: ${this.configService.get<string>(
+      'DB_NAME',
+    )}\n- DB_HOST: ${this.configService.get<string>('DB_HOST')}`;
   }
 }
